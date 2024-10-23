6afe60352079cf0158adf41f56c556e54a9252b1
BerkanGH <berkankeskin28@gmail.com>
2024-10-23 03:25:03 +0300

    odev

---
+++ PatikaEncap/PatikaEncap/Araba.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaEncap
+{
+    public class Araba
+    {
+
+        // araba klasında property leri oluşturduk ardından private olarakta kapısayisinin kontrollü şekilde ayarladım. Eğer 2 ya da 4 girilirse kapısayisi -1 olarak ayarlanacak ve kullanıcıya da mesaj gelcek
+
+        public string _model { get; set; }
+        public string _renk { get; set; }
+        public string _marka { get; set; }
+
+        private int _kapıSayisi;
+
+        public int KapıSayisi { 
+            
+            get { return _kapıSayisi;}
+
+            set { if (value == 2 || value == 4)
+                {
+
+                    _kapıSayisi = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("kapı sayısı 2 ya da 4 olabilir");
+                    _kapıSayisi = -1;
+                }
+            
+            }
+        }
+
+        public void BilgileriGöster()
+        {
+
+            Console.WriteLine($"modeli : {_model} rengi : {_renk} markası: {_marka} kapı sayısı: {_kapıSayisi} ");
+
+        }
+
+
+    }
+}
---
+++ PatikaEncap/PatikaEncap/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaEncap
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            //araba1 için değerleri girdik. Burada kapı sayısı 4 dedik ve herhangi bir hata almadan devam ediyor.
+
+            Araba araba1 = new Araba();
+
+            araba1._marka = "mercedes";
+            araba1.KapıSayisi = 4;
+            araba1._renk = "siyah";
+            araba1._model = "2015 model";
+
+
+            araba1.BilgileriGöster();
+
+            // burada kapı sayısına 3 dediğmiz için kullanıcıya geri bildirim veriyoruz ve araba.cs de kapısayısını -1 olarakta ayarladık.
+
+            Araba araba2 = new();
+
+            araba2._marka = "audi";
+            araba2.KapıSayisi = 3;
+            araba2._renk = "lacivert";
+            araba2._model = "2020";
+
+
+            araba2.BilgileriGöster();
+
+            
+            
+
+           
+
+
+
+        }
+
+    }
+}
